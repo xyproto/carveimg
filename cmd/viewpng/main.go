2b9d5dcdafe4a536663160eb6d21aecc89c43ced
Alexander F. Rødseth <rodseth@gmail.com>
2022-06-08 23:56:16 +0200

    Refactor

---
+++ cmd/viewpng/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/esimov/caire"
+	"github.com/xyproto/showyourself"
+	"github.com/xyproto/vt100"
+)
+
+const versionString = "viewpng 1.0.0"
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println(versionString)
+		fmt.Fprintln(os.Stderr, "Please supply a PNG filename")
+		os.Exit(1)
+	}
+
+	filename := os.Args[1]
+
+	// Initialize vt100 terminal settings
+	vt100.Init()
+
+	// Prepare a canvas
+	c := vt100.NewCanvas()
+
+	// Find the width and height of the canvas
+	width := int(c.Width())
+	height := int(c.Height())
+
+	// Prepare the content-aware image resizing
+	p := &caire.Processor{
+		BlurRadius:     1, // or ie. 4
+		SobelThreshold: 4, // or ie. 2
+		NewWidth:       width,
+		NewHeight:      height,
+		Percentage:     false,
+		Square:         false,
+		Debug:          false,
+		//Preview:        true, // Show GUI window, may cause errors related to context or ctx.Px
+		FaceDetect: true,
+		//FaceAngle:      0.0,
+		//MaskPath:       "",
+		//RMaskPath:      "",
+		ShapeType: "circle",
+		//SeamColor:      "#ff0000",
+	}
+
+	// Load the given filename
+	nImage, err := showyourself.LoadImage(filename)
+	if err != nil {
+		vt100.Close()
+		fmt.Fprintf(os.Stderr, "Could not load %s: %s\n", filename, err)
+		os.Exit(1)
+	}
+
+	// Content aware resizing
+	resizedImage, err := p.Resize(nImage)
+	if err != nil {
+		vt100.Close()
+		fmt.Fprintf(os.Stderr, "Could not resize image: %s\n", err)
+		os.Exit(1)
+
+	}
+
+	// Draw the image to the canvas, using only the basic 16 colors
+	if err := showyourself.Draw(c, resizedImage); err != nil {
+		vt100.Close()
+		fmt.Fprintln(os.Stderr, "Could not draw image: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Output the filename on top of the image
+	//c.Write(10, 10, vt100.LightBlue, vt100.BackgroundDefault, filename)
+
+	// Draw the contents of the canvas to the screen
+	c.Draw()
+
+	// Wait for a keypress
+	vt100.WaitForKey()
+
+	// Reset the vt100 terminal settings
+	vt100.Close()
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
-package main
+package showyourself
 
 import (
-	_ "embed"
 	"errors"
 	"fmt"
 	"image"
@@ -9,14 +8,13 @@ import (
 	"image/png"
 	"os"
 
-	"github.com/esimov/caire"
 	"github.com/xyproto/palgen"
 	"github.com/xyproto/vt100"
 )
 
-const drawRune = '▒'
+var DrawRune = '▒'
 
-func convertToNRGBA(img image.Image) (*image.NRGBA, error) {
+func ConvertToNRGBA(img image.Image) (*image.NRGBA, error) {
 	nImage := image.NewNRGBA(img.Bounds())
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
@@ -45,7 +43,7 @@ func LoadImage(filename string) (*image.NRGBA, error) {
 	if nImage, ok := img.(*image.NRGBA); ok {
 		return nImage, nil
 	}
-	return convertToNRGBA(img)
+	return ConvertToNRGBA(img)
 }
 
 // Draw attempts to draw the given image.Image onto a VT100 Canvas
@@ -104,82 +102,8 @@ func Draw(canvas *vt100.Canvas, m image.Image) error {
 				}
 			}
 			// Draw the "pixel" on the canvas using the vc color and the draw rune
-			canvas.PlotColor(uint(x), uint(y), vc, drawRune)
+			canvas.PlotColor(uint(x), uint(y), vc, DrawRune)
 		}
 	}
 	return nil
-}
-
-func main() {
-	if len(os.Args) <= 1 {
-		fmt.Fprintln(os.Stderr, "Please supply a PNG filename")
-		os.Exit(1)
-	}
-
-	filename := os.Args[1]
-
-	// Initialize vt100 terminal settings
-	vt100.Init()
-
-	// Prepare a canvas
-	c := vt100.NewCanvas()
-
-	// Find the width and height of the canvas
-	width := int(c.Width())
-	height := int(c.Height())
-
-	// Prepare the content-aware image resizing
-	p := &caire.Processor{
-		BlurRadius:     1, // or ie. 4
-		SobelThreshold: 4, // or ie. 2
-		NewWidth:       width,
-		NewHeight:      height,
-		Percentage:     false,
-		Square:         false,
-		Debug:          false,
-		//Preview:        true, // Show GUI window, may cause errors related to context or ctx.Px
-		FaceDetect: true,
-		//FaceAngle:      0.0,
-		//MaskPath:       "",
-		//RMaskPath:      "",
-		ShapeType: "circle",
-		//SeamColor:      "#ff0000",
-	}
-
-	// Load the given filename
-	nImage, err := LoadImage(filename)
-	if err != nil {
-		vt100.Close()
-		fmt.Fprintf(os.Stderr, "Could not load %s: %s\n", filename, err)
-		os.Exit(1)
-	}
-
-	// Content aware resizing
-	resizedImage, err := p.Resize(nImage)
-	if err != nil {
-		vt100.Close()
-		fmt.Fprintf(os.Stderr, "Could not resize image: %s\n", err)
-		os.Exit(1)
-
-	}
-
-	// Draw the image to the canvas, using only the basic 16 colors
-	if err := Draw(c, resizedImage); err != nil {
-		vt100.Close()
-		fmt.Fprintln(os.Stderr, "Could not draw image: %s\n", err)
-		os.Exit(1)
-	}
-
-	// Output the filename on top of the image
-	//c.Write(10, 10, vt100.LightBlue, vt100.BackgroundDefault, filename)
-
-	// Draw the contents of the canvas to the screen
-	c.Draw()
-
-	// Wait for a keypress
-	vt100.WaitForKey()
-
-	// Reset the vt100 terminal settings
-	vt100.Close()
-
 }
