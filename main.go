38c5f8f5fed1104f2f20e0501cf1c8a5dd237330
Alexander F. Rødseth <rodseth@gmail.com>
2022-06-08 17:48:02 +0200

    Various

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
+	"image"
+	"image/color"
 	"image/png"
 	"os"
 
+	"github.com/esimov/caire"
 	"github.com/xyproto/vt100"
 )
 
@@ -15,43 +18,90 @@ var (
 	grumpyCat []byte
 )
 
-func Load() error {
+func LoadEmbeddedImage() (image.Image, error) {
 	// Decode the image
 	reader := bytes.NewReader(grumpyCat)
 	img, err := png.Decode(reader)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	fmt.Printf("img %T\n", img)
-	return nil
+	return img, nil
 }
 
 func main() {
-	if len(grumpyCat) == 0 {
-		fmt.Fprintln(os.Stderr, "Embed error")
-		os.Exit(1)
-	}
-
-	if err := Load(); err != nil {
-		fmt.Fprintf(os.Stderr, "Could not decode grumpy-cat.png: %s\n\n", err.Error())
-		os.Exit(1)
-	}
-
 	// Initialize vt100 terminal settings
 	vt100.Init()
 
 	// Prepare a canvas
 	c := vt100.NewCanvas()
 
-	// Draw things on the canvas
-	c.Plot(10, 10, '!')
-	c.Write(12, 12, vt100.LightGreen, vt100.BackgroundDefault, "hi")
-	c.Write(15, 15, vt100.White, vt100.BackgroundMagenta, "floating")
-	c.PlotColor(12, 17, vt100.LightRed, '*')
-	c.PlotColor(10, 20, vt100.LightBlue, 'ø')
-	c.PlotColor(11, 20, vt100.LightBlue, 'l')
+	// Find the width and height of the canvas
+	width := c.Width()
+	height := c.Height()
 
-	c.WriteString(10, 21, vt100.White, vt100.BackgroundRed, "øl")
+	proc := &caire.Processor{
+		BlurRadius:     4, // or ie. 1
+		SobelThreshold: 2, // or ie. 4
+		NewWidth:       width,
+		NewHeight:      height,
+		Percentage:     false,
+		Square:         false,
+		Debug:          false,
+		//Preview:        true, // Show GUI window, may cause errors related to context or ctx.Px
+		//FaceDetect:     false,
+		//FaceAngle:      0.0,
+		//MaskPath:       "",
+		//RMaskPath:      "",
+		//ShapeType:      "circle",
+		//SeamColor:      "#ff0000",
+	}
+
+	if len(grumpyCat) == 0 {
+		fmt.Fprintln(os.Stderr, "Embed error")
+		os.Exit(1)
+	}
+
+	img, err := LoadEmbeddedImage()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not decode grumpy-cat.png: %s\n\n", err.Error())
+		os.Exit(1)
+	}
+
+	// 	var buf bytes.Buffer
+	// 	reader := bytes.NewReader(grumpyCat)
+	// 	err := proc.Process(&buf, reader)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "Could not process grumpy-cat.png: %s\n\n", err.Error())
+	// 		os.Exit(1)
+	// 	}
+
+	resizedImage, err := proc.Resize(img)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not resize grumpy-cat.png: %s\n\n", err.Error())
+		os.Exit(1)
+	}
+
+	c.Write(12, 12, vt100.LightGreen, vt100.BackgroundDefault, "grumpy-cat.png")
+
+	// 	for y := 0; y < height; y++ {
+	// 		for x := 0; x < width; x++ {
+	// 		}
+	// 	}
+
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+
+			c := color.NRGBAModel.Convert(img.At(x, y)).(color.NRGBA)
+			r := int(c.R)
+			g := int(c.G)
+			b := int(c.B)
+			alpha := int(c.A) // TODO: assume a black background and multiply in the alpha
+			fmt.Println(r, g, b, alpha)
+		}
+	}
+
+	// Draw things on the canvas
+	c.PlotColor(12, 17, vt100.LightRed, '*')
 
 	// Draw the contents of the canvas
 	c.Draw()
@@ -61,4 +111,5 @@ func main() {
 
 	// Reset the vt100 terminal settings
 	vt100.Close()
+
 }
