23936dd9303ba23076c76032a4a4587d652c7c41
Alexander F. Rødseth <rodseth@gmail.com>
2023-01-31 13:42:44 +0100

    New patch release

--- cmd/img/main.go
+++ cmd/img/main.go
@@ -11,7 +11,7 @@ import (
 	"golang.org/x/image/draw"
 )
 
-const versionString = "img 1.2.1"
+const versionString = "img 1.2.2"
 
 // var imageResizeFunction = draw.NearestNeighbor
 // var imageResizeFunction = draw.ApproxBiLinear
